5d4c7ca2784d22a6b9fabc61808332c8d91131d7
Nithyanandan-lmsace <nithyanandan.lmsace@gmail.com>
2022-05-17 16:47:25 +0530

    Footer and drawer responsive backdrop fixed version and readme file updated

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2022051600; // This component release level.
+$plugin->version    = 2022051700; // This component release level.
 $plugin->maturity   = MATURITY_STABLE; // This version's maturity level.
 $plugin->release    = 'v4.0'; // This version's release version.
 $plugin->requires   = 2022041900; // This version's moodle require release.
