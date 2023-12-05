42a45b7fc3852062356ed3095ba68fd4c0d59603
Prasanna LMSACE <prasanna_t@lmsace.com>
2023-12-05 11:42:54 +0530

    Version increased

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version    = 2023102400; // This component release level.
+$plugin->version    = 2023102401; // This component release level.
 $plugin->maturity   = MATURITY_STABLE; // This version's maturity level.
-$plugin->release    = 'v4.3'; // This version's release version.
+$plugin->release    = 'v4.3.1'; // This version's release version.
 $plugin->requires   = 2022041200; // This version's moodle require release.
 $plugin->component  = 'theme_academi'; // This component type_name.
 $plugin->dependencies = [
